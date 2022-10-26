e66a1dc8457676ac04c639c9c525a385b54addfa
Renato Adorno <renattoadorno@gmail.com>
2022-10-26 17:45:23 -0300

    ✨ feat(controller): created func CreateUser and GetUser

---
+++ api-users/controllers/user.controller.go
@@ -0,0 +1,46 @@
+package controllers
+
+import (
+	"api-users/models"
+	"api-users/responses"
+	"api-users/services"
+	"context"
+	"github.com/go-playground/validator/v10"
+	"github.com/labstack/echo/v4"
+)
+
+var (
+	ctx  context.Context = context.TODO()
+	user models.Users
+)
+
+func CreateUser(c echo.Context) error {
+	err := c.Bind(&user)
+	if err != nil {
+		return c.JSON(responses.BadRequest(err))
+	}
+
+	validate := validator.New()
+	if validationErr := validate.Struct(&user); validationErr != nil {
+		return c.JSON(responses.BadRequest(err))
+	}
+
+	result, err := services.Insert(ctx, user)
+	if err != nil {
+		return c.JSON(responses.InternalError(err))
+	}
+
+	return c.JSON(responses.Created(result))
+}
+
+func GetUser(c echo.Context) error {
+	userId := c.Param("userId")
+
+	result, err := services.Get(ctx, userId, user)
+
+	if err != nil {
+		return c.JSON(responses.InternalError(err))
+	}
+
+	return c.JSON(responses.Ok(result))
+}
